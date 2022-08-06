702ccf90f4e9120a2d0b6bf22d91b7ca515d1dd5
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-08-06 17:14:08 +0530

    Create Ass_p_2_5_main.java

---
+++ Ass_p_2_5_main.java
@@ -0,0 +1,12 @@
+public class Ass_p_2_5_main {
+
+        public static void main(String[] args) {
+            Ass_p_2_5 p1=new Ass_p_2_5();
+            Ass_p_2_5 sc1= new Ass_p_2_5();
+
+            System.out.println("The average of the numbers is :"+ sc1.getaveragee(2));    // pass the argumnet
+            System.out.println("The average of the numbers is :"+sc1.getaverage(2,3)); // pass the argumnet
+            System.out.println("The average of the numbers is :"+sc1.getaverage(2,3,5));  // pass the argumnet
+        }
+}
+
