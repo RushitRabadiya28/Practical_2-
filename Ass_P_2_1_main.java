a219cb33a4829e95adf917b9ad640c42ce4c676f
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-08-06 14:36:38 +0530

    Update Ass_P_2_1_main.java

--- Ass_P_2_1_main.java
+++ Ass_P_2_1_main.java
@@ -4,12 +4,12 @@ public class Ass_P_2_1_main {
 
 
         public static void main(String[] args) {
-            Ass_p_2_1 object1 = new Ass_p_2_1();
+            Ass_p_2_1 object1 = new Ass_p_2_1(); // create object of class file 
             System.out.print("Enter The Argument for Radius -> ");
             Scanner sc = new Scanner(System.in);
-            double a = sc.nextDouble();
+            double a = sc.nextDouble();  // take a input from user 
 
-            Ass_p_2_1 object2 = new Ass_p_2_1(a);
+            Ass_p_2_1 object2 = new Ass_p_2_1(a); // create  2ed object  of class file with parameter 
 
             System.out.println("Area and Perimeter of Circle for default argument 1 : "+object1.getArea()+" and "+object1.getPerimeter());
             System.out.println("Area and Perimeter of Circle for argument  " + a + "  : "+object2.getArea()+" and "+object2.getPerimeter());
