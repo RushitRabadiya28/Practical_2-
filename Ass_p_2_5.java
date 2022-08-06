6eb41309238ebf22139412f1fbdcc9b3d1947770
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-08-06 17:07:55 +0530

    Create Ass_p_2_5.java

---
+++ Ass_p_2_5.java
@@ -0,0 +1,21 @@
+public class Ass_p_2_5
+{
+        float getaveragee(float a)   // getaverage method find average
+     {      float avg;
+            avg=a;
+            return avg;
+        }
+        float getaverage(float a,float b)   // getaverage method find average
+     {   
+            float avg;
+            avg=(a+b)/2;
+            return avg;
+        }
+        float getaverage(float a,float b,float c)     // getaverage method find average
+        {
+            float avg;
+            avg=(a+b+c)/3;
+            return avg;
+
+        }
+}
