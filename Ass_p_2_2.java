2d2ded9782f1fbfd7704f59617fc5ce4757751e8
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-08-06 16:51:23 +0530

    Update Ass_p_2_2.java

--- Ass_p_2_2.java
+++ Ass_p_2_2.java
@@ -6,65 +6,71 @@ public class Ass_p_2_2 {
     private double annual=07;
     private Date date = new Date();
 
-    Ass_p_2_2(){}
-    Ass_p_2_2(int id ,double balance)
+    Ass_p_2_2(){}   //  no-arg constructor 
+    Ass_p_2_2(int id ,double balance)    // parameterized constructor 
     {
         this.id=id;
         this.balance=balance;
     }
 
-    public int getId() {
+    public int getId() //  getid method  for get id
+{
         return id;
     }
 
-    public double getBalance() {
+    public double getBalance() //  getbalance method for get balance 
+{
         return balance;
     }
 
-    public double getAnnual() {
+    public double getAnnual() //  getannual method for get annual 
+{
         return annual;
     }
 
-    public Date getDate() {
+    public Date getDate() // for get date 
+{
         return date;
     }
 
-    public void setId(int id) {
+    public void setId(int id) // setid method for set id 
+{
 
         this.id = id;
     }
 
-    public void setBalance(double balance) {
+    public void setBalance(double balance)  // setbalance method for set balance 
+ {
         this.balance = balance;
     }
 
-    public void setAnnual(double annual) {
+    public void setAnnual(double annual) // setannual method for set annual 
+ {
         this.annual = annual;
     }
 
-    public void setDate(Date date) {
+    public void setDate(Date date) //  setdate method  for set date 
+ {
         this.date = date;
     }
-    public double getMonthlyrate()
+    public double getMonthlyrate()  // getMonthlyrate method for  find mounthlyrate 
     {
         return  annual/12;
 
     }
-    public double getMonthlyinterest()
+    public double getMonthlyinterest()  // getMonthlyinterest method for find mounthlyinterest 
     {
         return balance*(annual/1200);
     }
-    public double withdraw(double a)
+    public double withdraw(double a)  // withdraw method 
 
     {
         balance=balance-a;
         return balance;
     }
-    public double deposit(double a)
+    public double deposit(double a)  // deposit method 
     {
         balance=balance+a;
         return balance;
     }
 }
-
-
