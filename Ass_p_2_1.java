c352a62d90935fc000a0087b608cfbc303ea4803
RushitRabadiya28 <110188571+RushitRabadiya28@users.noreply.github.com>
2022-08-06 14:32:24 +0530

    Update Ass_p_2_1.java

--- Ass_p_2_1.java
+++ Ass_p_2_1.java
@@ -1,16 +1,20 @@
 public class Ass_p_2_1 {
         private double radius;
 
-        public Ass_p_2_1() {
+        public Ass_p_2_1() //no-argumentconstructo  default valueis1 
+        {
             radius = 1;
         }
-        public Ass_p_2_1(double radius) {
+        public Ass_p_2_1(double radius) //Single argument constructor 
+        {
             this.radius = radius;
         }
-        public double getArea(){
+        public double getArea() // getarea method 
+        {
             return 22*radius*radius/7;
         }
-        public double getPerimeter(){
+        public double getPerimeter() // getparimeter  method 
+        {
             return 2*22*radius/7;
         }
 
